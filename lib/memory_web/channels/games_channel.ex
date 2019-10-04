81f08cc75dc29c9585f8f0640f7832e8bcf59199
weihanLiu <liu.weih@husky.neu.edu>
2019-10-04 17:46:31 -0400

    add channel.ex

--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -1,19 +1,35 @@
 defmodule MemoryWeb.GamesChannel do
   use MemoryWeb, :channel
 
-  def join("games:lobby", payload, socket) do
+  alias Memory.Game
+
+  def join("games:" <> name, payload, socket) do
     if authorized?(payload) do
-      {:ok, socket}
+      game = Game.new()
+      socket = socket
+      |> assign(:game, game)
+      |> assign(:name, name)
+      {:ok, %{"join" => name, "game" => Game.client_view(game)}, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
+
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
+  def handle_in("click", %{"click" => i}, socket) do
+    game = Game.click(socket.assigns[:game], i) do
+    socket = assign(socket, :game, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
+    end
+    
+  def handle_in("restart", payload, socket) do
+    game = Game.new()
+    socket = socket |>assign(:game, game)
+    {:reply, {:ok, %{"game" => Game.client_view(game)}}, socket}
   end
+
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (games:lobby).
